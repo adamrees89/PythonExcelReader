c4fc06b0be939f27a8b2d4c35097b3363cd71d64
Adam Rees <adamrees89@googlemail.com>
2017-08-21 16:59:46 +0100

    Added tests
    Creates a .travis.yml file, including requirements.txt, and unit tests

---
+++ testfile.py
@@ -0,0 +1,16 @@
+import unittest
+import templateReader
+
+class openExcelFile(unittest.TestCase):
+	""" Class doc """
+	
+	def testFoo(self):
+		""" Class initialiser """
+		self.failUnless(False)
+
+def main():
+	unittest.main()
+
+if __name__ == '__main__':
+	main()
+
