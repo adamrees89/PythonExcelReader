e9e5ad86b251209dc20b91210b978e013ff08ab0
Adam Rees <adamrees89@googlemail.com>
2017-08-21 15:56:10 +0100

    Initial Commit
    Working files, and example excel files

---
+++ CreateSSExcelDoc.py
@@ -0,0 +1,27 @@
+#Need this packages
+import openpyxl #For interacting with Excel
+import sys #For interacting with the OS
+import time #For logging
+import logging #For logging
+import os
+from tkinter import filedialog
+from tkinter import *
+
+#Logging set-up information, check if the log directory exists, if not create it then set up the log
+now=time.strftime("%c")
+os.makedirs("logs",exist_ok=True)
+logging.basicConfig(filename='logs/SSExcelDocCreate.log',level=logging.DEBUG)
+logging.info('\nDate and Time:\n' + now + '\n-------------------------------\n')
+
+#Create the excel file
+wb=openpyxl.Workbook()
+f = Tk()
+f = filedialog.asksaveasfilename(initialdir = "%USERPROFILE%\Desktop",title = "Select save location",filetypes=[("Excel Workbook",".xlsx")],initialfile="Steady State Heating and Cooling Loads",defaultextension=".xlsx")
+
+#Try to save, but catch the error if the user hits cancel.
+try:
+	wb.save(f)
+except Exception:
+	logging.critical("SaveAs dialogue closed with 'Cancel', cannot continue")
+	exit(5)
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PythonExcelReader
 Python scripts to read an excel file and print the contents to a SQLlite3 database.  Future work involves re-writing that excel file elsewhere
+
+#CreateSSExcelDoc.py
+
+This crates an excel document based on the database created by templateReader.py
